2b470e111061bac1dfd8394f1d05099415eb7acf
eggnog0425 <haruyo.coffee@gmail.com>
2022-03-06 13:53:55 +0900

    デフォルト

---
+++ HanahirakebaFuuuOshi/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace HanahirakebaFuuuOshi;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ HanahirakebaFuuuOshi/State/ReloadbleState.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HanahirakebaFuuuOshi.State {
+    public class ReloadbleState {
+        private bool IsReloadble { get; set; }
+    }
+}
